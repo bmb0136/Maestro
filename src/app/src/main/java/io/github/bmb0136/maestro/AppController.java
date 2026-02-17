44b666b8597df5dc40efde955cc33de15eae3e57
Brandon Buckley <brandonmb0136@gmail.com>
2026-02-17 13:26:07 -0600

    Add ability to scroll/drag BPM value

--- src/app/src/main/java/io/github/bmb0136/maestro/AppController.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/AppController.java
@@ -5,12 +5,16 @@ import io.github.bmb0136.maestro.core.event.RemoveTrackFromTimelineEvent;
 import io.github.bmb0136.maestro.core.timeline.Timeline;
 import io.github.bmb0136.maestro.core.timeline.TimelineManager;
 import io.github.bmb0136.maestro.core.timeline.Track;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import org.jetbrains.annotations.NotNull;
@@ -31,7 +35,10 @@ public class AppController {
     private VBox trackClipList;
     @FXML
     private Parent root;
+    @FXML
+    private Label bpmLabel;
     private final TimelineManager manager = new TimelineManager(1024, new Timeline());
+    private final SimpleIntegerProperty bpm = new SimpleIntegerProperty(120);
 
     @FXML
     private void initialize() {
@@ -46,11 +53,34 @@ public class AppController {
             GridPane.setRowSpan(centerColumn, newValue ? 2 : 3);
         });
         centerScrollBar.setVisible(false);
+
+        bpmLabel.textProperty().bind(bpm.map(value -> "BPM: " + value));
     }
 
     @FXML
     private void onAddTrackButtonClicked() {
         addTrack(new Track());
+    }
+
+    @FXML
+    private void onBpmScrolled(ScrollEvent event) {
+        if (Math.abs(event.getDeltaY()) < 1e-6) {
+            return;
+        }
+        int delta = event.getDeltaY() >= 0 ? 1 : -1;
+        bpm.set(bpm.get() + delta);
+    }
+
+    private double bpmDragStartY;
+    private int bpmDragStartValue;
+    @FXML
+    private void onBpmDragged(MouseEvent event) {
+        if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+            bpmDragStartY = event.getScreenY();
+            bpmDragStartValue = bpm.get();
+        } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+            bpm.set((int) (bpmDragStartValue - (0.25 * (event.getScreenY() - bpmDragStartY))));
+        }
     }
 
     private void addTrack(@NotNull Track track) {
