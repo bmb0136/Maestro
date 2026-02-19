10da143fa20997a0ffcd7a6bdf04e174a0c99b84
Brandon Buckley <brandonmb0136@gmail.com>
2026-02-19 15:38:04 -0600

    Inline shell into flake

--- flake.nix
+++ flake.nix
@@ -4,15 +4,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { self', pkgs, ... }: {
-        packages.default = self'.packages.maestro;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { self', pkgs, ... }:
+        {
+          packages.default = self'.packages.maestro;
 
-        packages.maestro = pkgs.callPackage ./src {};
+          packages.maestro = pkgs.callPackage ./src { };
 
-        devShells.default = pkgs.callPackage ./src/shell.nix {};
-      };
+          devShells.default = pkgs.mkShell {
+            packages = [
+              (pkgs.javaPackages.compiler.openjdk21.override { enableJavaFX = true; })
+            ];
+          };
+        };
     };
 }
