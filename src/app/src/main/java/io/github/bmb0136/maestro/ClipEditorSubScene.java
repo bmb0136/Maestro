90bceee29e911691a25f0530774c0f398f20dc69
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-03 22:23:02 -0600

    Update ClipEditorSubScene and PianoRollEditorSubScene to use event targeting system for update

--- src/app/src/main/java/io/github/bmb0136/maestro/ClipEditorSubScene.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/ClipEditorSubScene.java
@@ -1,18 +1,22 @@
 package io.github.bmb0136.maestro;
 
 import io.github.bmb0136.maestro.core.clip.Clip;
+import io.github.bmb0136.maestro.core.event.EventTarget;
 import io.github.bmb0136.maestro.core.timeline.TimelineManager;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.scene.SubScene;
 import javafx.scene.layout.Pane;
 
 import java.util.UUID;
 
-public abstract class ClipEditorSubScene<T extends Clip> extends SubScene {
+public abstract class ClipEditorSubScene<T extends Clip> extends SubScene implements AutoCloseable {
 
-    protected final SimpleObjectProperty<T> clip;
     protected final TimelineManager manager;
     protected final UUID trackId, clipId;
+    private final AutoCloseable callback;
+    private final ReadOnlyObjectWrapper<T> clipWrapper;
+    protected final ReadOnlyObjectProperty<T> clip;
 
     public ClipEditorSubScene(TimelineManager manager, UUID trackId, UUID clipId) {
         // Dummy node (can't pass null here)
@@ -22,6 +26,31 @@ public abstract class ClipEditorSubScene<T extends Clip> extends SubScene {
         this.trackId = trackId;
         this.clipId = clipId;
         //noinspection unchecked
-        clip = new SimpleObjectProperty<>((T) manager.get().getTrack(trackId).flatMap(t -> t.getClip(clipId)).orElseThrow());
+        clipWrapper = new ReadOnlyObjectWrapper<>((T) manager.get().getTrack(trackId).flatMap(t -> t.getClip(clipId)).orElseThrow());
+        clip = clipWrapper.getReadOnlyProperty();
+        callback = manager.registerChangeCallback(this::changeCallback);
+    }
+
+    private void changeCallback(EventTarget target) {
+        if (!target.isClip()) {
+            return;
+        }
+        var tTrackId = target.getTrackId();
+        if (tTrackId.isEmpty() || !tTrackId.get().equals(trackId)) {
+            return;
+        }
+        var tClipId = target.getClipId();
+        if (tClipId.isEmpty() || !tClipId.get().equals(clipId)) {
+            return;
+        }
+        // HACK: copying since ObjectPropertyBase won't trigger updates if the instance is the same
+        //noinspection unchecked
+        var clip = (T) target.getTimeline().getTrack(trackId).flatMap(t -> t.getClip(clipId)).orElseThrow().copy(false);
+        clipWrapper.set(clip);
+    }
+
+    @Override
+    public void close() throws Exception {
+        callback.close();
     }
 }
