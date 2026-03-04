6834f0854f254ff81b3d58ffd11e49db1a5cca31
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-03 22:22:40 -0600

    Add timeline ref to EventTarget

--- src/core/src/main/java/io/github/bmb0136/maestro/core/event/EventTarget.java
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/event/EventTarget.java
@@ -11,13 +11,16 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class EventTarget {
+    @NotNull
+    private final Timeline timeline;
     private final Kind kind;
     @Nullable
     private final UUID trackId;
     @Nullable
     private final UUID clipId;
 
-    private EventTarget(@NotNull Kind kind, @Nullable UUID trackId, @Nullable UUID clipId) {
+    private EventTarget(@NotNull Kind kind, @NotNull Timeline timeline, @Nullable UUID trackId, @Nullable UUID clipId) {
+        this.timeline = timeline;
         this.kind = kind;
         this.trackId = trackId;
         this.clipId = clipId;
@@ -35,6 +38,11 @@ public class EventTarget {
         return kind == Kind.CLIP;
     }
 
+    @NotNull
+    public Timeline getTimeline() {
+        return timeline;
+    }
+
     public Optional<UUID> getTrackId() {
         return Optional.ofNullable(trackId);
     }
@@ -43,23 +51,23 @@ public class EventTarget {
         return Optional.ofNullable(clipId);
     }
 
-    public static EventTarget timeline() {
-        return new EventTarget(Kind.TIMELINE, null, null);
+    public static EventTarget timeline(@NotNull Timeline timeline) {
+        return new EventTarget(Kind.TIMELINE, timeline, null, null);
     }
 
-    public static EventTarget track(@NotNull UUID trackId) {
-        return new EventTarget(Kind.TRACK, trackId, null);
+    public static EventTarget track(@NotNull Timeline timeline, @NotNull UUID trackId) {
+        return new EventTarget(Kind.TRACK, timeline, trackId, null);
     }
 
-    public static EventTarget clip(@NotNull UUID trackId, @NotNull UUID clipId) {
-        return new EventTarget(Kind.CLIP, trackId, clipId);
+    public static EventTarget clip(@NotNull Timeline timeline, @NotNull UUID trackId, @NotNull UUID clipId) {
+        return new EventTarget(Kind.CLIP, timeline, trackId, clipId);
     }
 
     public static EventTarget fromEventContext(@NotNull EventContext<?> context) {
         return switch (context.target()) {
-            case Timeline ignored -> timeline();
-            case Track track -> track(track.getId());
-            case Clip clip -> clip(Objects.requireNonNull(context.track()).getId(), clip.getId());
+            case Timeline target -> timeline(target);
+            case Track track -> track(Objects.requireNonNull(context.timeline()), track.getId());
+            case Clip clip -> clip(Objects.requireNonNull(context.timeline()), Objects.requireNonNull(context.track()).getId(), clip.getId());
             default -> throw new IllegalArgumentException("Unknown EventContext.target() value: " + context.target().getClass().getName());
         };
     }
