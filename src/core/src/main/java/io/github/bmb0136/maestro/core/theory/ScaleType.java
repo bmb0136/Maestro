b02806e4a254b42cc05408ff0af2ed31c6da3986
Brandon Buckley <159852919+bmb0136@users.noreply.github.com>
2026-03-10 18:47:17 -0500

    Add new music theory APIs to core project (#38)
    * #32: Add Scale class
    
    * #32: Add ScaleFactory and ScaleType
    
    * Replace javafx.util.Pair with custom Tuple2 class
    
    * Add extra PitchName functions (needed for Chords)
    
    * Ensure ScaleFactory generates scales w/ correct enharmonics
    
    * #33: Add Chord and related classes

---
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/theory/ScaleType.java
@@ -0,0 +1,22 @@
+package io.github.bmb0136.maestro.core.theory;
+
+public enum ScaleType {
+    // All 7 modes
+    IONIAN, MAJOR, // Same thing
+    DORIAN,
+    PHRYGIAN,
+    LYDIAN,
+    MIXOLYDIAN,
+    AEOLIAN, MINOR, // Same thing
+    LOCRIAN,
+    // Pentatonic
+    MAJOR_PENTATONIC,
+    MINOR_PENTATONIC,
+    // Diminished
+    WHOLE_HALF_DIMINISHED,
+    HALF_WHOLE_DIMINISHED,
+    // Misc.
+    CHROMATIC,
+    WHOLE_TONE,
+    HARMONIC_MINOR
+}
---
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/util/Tuple2.java
@@ -0,0 +1,4 @@
+package io.github.bmb0136.maestro.core.util;
+
+public record Tuple2<T1, T2>(T1 first, T2 second) {
+}
