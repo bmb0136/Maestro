a3499c9fdda59f84289b434d837b880ad1454ea4
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-14 23:24:23 -0400

    Make RemoveModifierFromClipEvent also support removing by index

--- src/core/src/main/java/io/github/bmb0136/maestro/core/event/RemoveModifierFromClipEvent.java
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/event/RemoveModifierFromClipEvent.java
@@ -1,20 +1,36 @@
 package io.github.bmb0136.maestro.core.event;
 
 import io.github.bmb0136.maestro.core.clip.Clip;
+import io.github.bmb0136.maestro.core.modifier.ModifierList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
 public class RemoveModifierFromClipEvent extends ClipEvent {
+    @Nullable
     private final UUID modifierId;
+    private final int modifierIndex;
 
-    public RemoveModifierFromClipEvent(UUID trackId, UUID clipId, UUID modifierId) {
+    public RemoveModifierFromClipEvent(UUID trackId, UUID clipId, @NotNull UUID modifierId) {
         super(trackId, clipId);
         this.modifierId = modifierId;
+        modifierIndex = -1;
+    }
+
+    public RemoveModifierFromClipEvent(UUID trackId, UUID clipId, int modifierIndex) {
+        super(trackId, clipId);
+        modifierId = null;
+        this.modifierIndex = modifierIndex;
     }
 
     @Override
     public EventResult apply(@NotNull EventContext<Clip> context) {
-        return context.target().getModifiers().removeModifier(modifierId) ? EventResult.OK : EventResult.NOOP;
+        ModifierList modifiers = context.target().getModifiers();
+        int index = modifierIndex;
+        if (modifierId != null) {
+            index = modifiers.indexOf(modifierId).orElse(-1);
+        }
+        return modifiers.removeModifier(index) ? EventResult.OK : EventResult.NOOP;
     }
 }
