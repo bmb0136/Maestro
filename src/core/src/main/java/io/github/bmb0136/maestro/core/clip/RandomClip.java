1f1b91ab1aa319996ab0fcf6576a20a6c4faddb2
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-24 23:08:33 -0500

    #43: Add main RandomClip arguments

--- src/core/src/main/java/io/github/bmb0136/maestro/core/clip/RandomClip.java
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/clip/RandomClip.java
@@ -1,12 +1,18 @@
 package io.github.bmb0136.maestro.core.clip;
 
 import io.github.bmb0136.maestro.core.theory.Note;
+import io.github.bmb0136.maestro.core.theory.PitchName;
+import io.github.bmb0136.maestro.core.theory.ScaleType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 import java.util.UUID;
 
 public class RandomClip extends Clip {
+    private ScaleType scale = ScaleType.MAJOR;
+    private PitchName rootPitch = PitchName.C;
+    private int rootOctave = 4;
+    private int minDegree = 0, maxDegree = 7;
 
     public RandomClip() {
         this(UUID.randomUUID());
@@ -15,6 +21,62 @@ public class RandomClip extends Clip {
     protected RandomClip(UUID id) {
         super(id);
     }
+
+    public ScaleType getScale() {
+        return scale;
+    }
+
+    public void setScale(ScaleType scale) {
+        if (!isMutable()) {
+            throw new IllegalStateException("RandomClip is immutable");
+        }
+        this.scale = scale;
+    }
+
+    public PitchName getRootPitch() {
+        return rootPitch;
+    }
+
+    public void setRootPitch(PitchName rootPitch) {
+        if (!isMutable()) {
+            throw new IllegalStateException("RandomClip is immutable");
+        }
+        this.rootPitch = rootPitch;
+    }
+
+    public int getRootOctave() {
+        return rootOctave;
+    }
+
+    public void setRootOctave(int rootOctave) {
+        if (!isMutable()) {
+            throw new IllegalStateException("RandomClip is immutable");
+        }
+        this.rootOctave = rootOctave;
+    }
+
+    public int getMinDegree() {
+        return minDegree;
+    }
+
+    public void setMinDegree(int minDegree) {
+        if (!isMutable()) {
+            throw new IllegalStateException("RandomClip is immutable");
+        }
+        this.minDegree = minDegree;
+    }
+
+    public int getMaxDegree() {
+        return maxDegree;
+    }
+
+    public void setMaxDegree(int maxDegree) {
+        if (!isMutable()) {
+            throw new IllegalStateException("RandomClip is immutable");
+        }
+        this.maxDegree = maxDegree;
+    }
+
 
     @Override
     protected Clip createCopy(boolean newId) {
