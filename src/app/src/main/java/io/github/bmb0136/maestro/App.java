13450d342c837d9b16ff3729071fd97343686975
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-27 17:39:55 -0500

    Call close() on AppController on app exit

--- src/app/src/main/java/io/github/bmb0136/maestro/App.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/App.java
@@ -10,13 +10,26 @@ import java.net.URL;
 import java.util.Objects;
 
 public class App extends Application {
+    private AutoCloseable controllerClosable = null;
+
     @Override
     public void start(Stage stage) throws Exception {
         URL resource = Objects.requireNonNull(getClass().getResource("/MainWindow.fxml"));
-        Parent root = FXMLLoader.load(resource);
+        FXMLLoader loader = new FXMLLoader(resource);
+        if (loader.getController() instanceof AutoCloseable closeable) {
+            controllerClosable = closeable;
+        }
+        Parent root = loader.load();
         Scene scene = new Scene(root, 1280, 720);
         stage.setScene(scene);
         stage.setTitle("Maestro");
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (controllerClosable != null) {
+            controllerClosable.close();
+        }
     }
 }
