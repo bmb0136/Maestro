a47fb6ae061ca1150ab64d4175f7b6fcb3c2b127
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-31 14:44:34 -0500

    #56: Add comments to ClipRenderer classes

--- src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/ChordClipRenderer.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/ChordClipRenderer.java
@@ -7,6 +7,14 @@ import javafx.scene.paint.Color;
 import org.jetbrains.annotations.NotNull;
 
 public class ChordClipRenderer {
+    /**
+     * Render the inside of the given clip onto the timeline
+     *
+     * @param clip The clip being rendered
+     * @param gc The {@link GraphicsContext} associated with the timeline canvas
+     * @param area The area of the timeline canvas to draw to
+     * @param baseColor The primary accent color to render things in
+     */
     public static void render(@NotNull ChordClip clip, @NotNull GraphicsContext gc, @NotNull Rectangle2D area, @NotNull Color baseColor) {
 
     }
