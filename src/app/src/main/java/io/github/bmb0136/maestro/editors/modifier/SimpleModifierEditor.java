b3790384dbc08c03ab61bc6ab018bd8dcbd740cc
Brandon Buckley <brandonmb0136@gmail.com>
2026-03-31 17:01:20 -0500

    #71: Add conditional visibility to SimpleModifierEditor

--- src/app/src/main/java/io/github/bmb0136/maestro/editors/modifier/SimpleModifierEditor.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/editors/modifier/SimpleModifierEditor.java
@@ -4,6 +4,7 @@ import io.github.bmb0136.maestro.core.modifier.Modifier;
 import io.github.bmb0136.maestro.core.timeline.TimelineManager;
 import io.github.bmb0136.maestro.core.util.BiHashMap;
 import javafx.beans.Observable;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -16,6 +17,8 @@ import java.util.UUID;
 import java.util.function.Consumer;
 
 public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubscene<T> {
+    private static final ObservableValue<Boolean> TRUE = new SimpleBooleanProperty(true);
+
     private final VBox components;
 
     public SimpleModifierEditor(TimelineManager manager, UUID trackId, UUID clipId, UUID modifierId) {
@@ -29,6 +32,10 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
     }
 
     protected void addInteger(String text, int min, int max, ObservableValue<Integer> getter, Consumer<Integer> valueChanged) {
+        addInteger(text, min, max, getter, valueChanged, TRUE);
+    }
+
+    protected void addInteger(String text, int min, int max, ObservableValue<Integer> getter, Consumer<Integer> valueChanged, ObservableValue<Boolean> visibleCondition) {
         var spinner = new Spinner<Integer>();
         var factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(min, max, getter.getValue());
         spinner.setValueFactory(factory);
@@ -39,10 +46,14 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
                 valueChanged.accept(newValue);
             }
         });
-        addLabeled(text, spinner);
+        addLabeled(text, spinner, visibleCondition);
     }
 
     protected void addFloat(String text, float min, float max, ObservableValue<Float> getter, Consumer<Float> valueChanged) {
+        addFloat(text, min, max, getter, valueChanged, TRUE);
+    }
+
+    protected void addFloat(String text, float min, float max, ObservableValue<Float> getter, Consumer<Float> valueChanged, ObservableValue<Boolean> visibleCondition) {
         var field = new TextField();
         getter.addListener((ignored1, ignored2, newValue) ->
                 field.setText(newValue.toString()));
@@ -58,10 +69,14 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
                 field.setText(getter.getValue().toString());
             }
         });
-        addLabeled(text, field);
+        addLabeled(text, field, visibleCondition);
     }
 
     protected <E extends Enum<E>> void addEnum(String text, BiHashMap<String, E> converter, List<String> order, ObservableValue<E> getter, Consumer<E> valueChanged) {
+        addEnum(text, converter, order, getter, valueChanged, TRUE);
+    }
+
+    protected <E extends Enum<E>> void addEnum(String text, BiHashMap<String, E> converter, List<String> order, ObservableValue<E> getter, Consumer<E> valueChanged, ObservableValue<Boolean> visibleCondition) {
         var box = new ChoiceBox<String>();
         order.forEach(box.getItems()::add);
         getter.addListener((ignored1, ignored2, newValue) ->
@@ -71,10 +86,14 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
                 valueChanged.accept(converter.get1(newValue));
             }
         });
-        addLabeled(text, box);
+        addLabeled(text, box, visibleCondition);
     }
 
     protected void addBoolean(String text, ObservableValue<Boolean> getter, Consumer<Boolean> valueChanged) {
+        addBoolean(text, getter, valueChanged, TRUE);
+    }
+
+    protected void addBoolean(String text, ObservableValue<Boolean> getter, Consumer<Boolean> valueChanged, ObservableValue<Boolean> visibleCondition) {
         var check = new CheckBox();
         getter.addListener((ignored1, ignored2, newValue) ->
                 check.setSelected(newValue));
@@ -83,10 +102,14 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
                 valueChanged.accept(newValue);
             }
         });
-        addLabeled(text, check);
+        addLabeled(text, check, visibleCondition);
     }
 
     protected void addButton(String text, Runnable onClick) {
+        addButton(text, onClick, TRUE);
+    }
+
+    protected void addButton(String text, Runnable onClick, ObservableValue<Boolean> visibleCondition) {
         var button = new Button(text);
         button.setOnAction(e -> onClick.run());
         button.maxWidthProperty().bind(components.widthProperty());
@@ -98,9 +121,10 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
 
         components.getChildren().add(hbox);
         hbox.heightProperty().addListener(this::recalculateHeight);
+        hbox.visibleProperty().bind(visibleCondition);
     }
 
-    protected void addLabeled(String text, Node node) {
+    protected void addLabeled(String text, Node node, ObservableValue<Boolean> visibleCondition) {
         HBox hbox = new HBox();
         hbox.setPadding(new Insets(0, 8, 0, 8));
         var children = hbox.getChildren();
@@ -117,6 +141,7 @@ public class SimpleModifierEditor<T extends Modifier> extends ModifierEditorSubs
         components.getChildren().add(hbox);
 
         hbox.heightProperty().addListener(this::recalculateHeight);
+        hbox.visibleProperty().bind(visibleCondition);
     }
 
     private void recalculateHeight(Observable ignored) {
