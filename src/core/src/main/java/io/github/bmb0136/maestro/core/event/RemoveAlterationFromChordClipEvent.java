02f008d51ba4cf1960c3728fc2e7cc31ddd3e8b3
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-10 14:54:19 -0500

    #49: Add RemoveAlterationFromChordClipEvent

---
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/event/RemoveAlterationFromChordClipEvent.java
@@ -0,0 +1,41 @@
+package io.github.bmb0136.maestro.core.event;
+
+import io.github.bmb0136.maestro.core.clip.ChordClip;
+import io.github.bmb0136.maestro.core.clip.Clip;
+import io.github.bmb0136.maestro.core.theory.Accidental;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class RemoveAlterationFromChordClipEvent extends ClipEvent {
+    private final Accidental accidental;
+    private final int scaleDegree;
+
+    public RemoveAlterationFromChordClipEvent(UUID trackId, UUID clipId, @Nullable Accidental accidental, int scaleDegree) {
+        super(trackId, clipId);
+        this.accidental = accidental;
+        this.scaleDegree = scaleDegree;
+    }
+
+    public RemoveAlterationFromChordClipEvent(UUID trackId, UUID clipId, int scaleDegree) {
+        this(trackId, clipId, null, scaleDegree);
+    }
+
+    @Override
+    public EventResult apply(@NotNull EventContext<Clip> context) {
+        if (!(context.target() instanceof ChordClip target)) {
+            return EventResult.WRONG_CLIP_TYPE;
+        }
+        var oldAlteration = target.getChordBuilderView().getAlteration(scaleDegree);
+        if (oldAlteration.map(a -> a == accidental).orElse(false)) {
+            return EventResult.NOOP;
+        }
+        if (accidental != null) {
+            target.removeAlteration(accidental, scaleDegree);
+        } else {
+            target.removeAlteration(scaleDegree);
+        }
+        return EventResult.OK;
+    }
+}
