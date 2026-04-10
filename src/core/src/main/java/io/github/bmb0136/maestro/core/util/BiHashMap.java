c494ed5ceff5347527622d9551fabcde7ee2c6ac
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-10 14:40:09 -0500

    Add remove1/2 + entries1/2 to BiHashMap

--- src/core/src/main/java/io/github/bmb0136/maestro/core/util/BiHashMap.java
+++ src/core/src/main/java/io/github/bmb0136/maestro/core/util/BiHashMap.java
@@ -1,6 +1,7 @@
 package io.github.bmb0136.maestro.core.util;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class BiHashMap<T1, T2> {
@@ -27,6 +28,24 @@ public class BiHashMap<T1, T2> {
         return true;
     }
 
+    public boolean remove1(T1 key1) {
+        var key2 = forward.remove(key1);
+        if (key2 != null) {
+            reverse.remove(key2);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remove2(T2 key2) {
+        var key1 = reverse.remove(key2);
+        if (key1 != null) {
+            forward.remove(key1);
+            return true;
+        }
+        return false;
+    }
+
     public boolean contains1(T1 v1) {
         return forward.containsKey(v1);
     }
@@ -49,5 +68,13 @@ public class BiHashMap<T1, T2> {
 
     public Set<T2> values2() {
         return reverse.keySet();
+    }
+
+    public Set<Map.Entry<T1, T2>> entries1() {
+        return forward.entrySet();
+    }
+
+    public Set<Map.Entry<T2, T1>> entries2() {
+        return reverse.entrySet();
     }
 }
