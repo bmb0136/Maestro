8c782d472dad40de6ff5de5d692912da81423e32
FGNation <trentsdatabase@gmail.com>
2026-04-10 08:14:18 -0500

    Scale Rendering: ModifierCount (Same code as Chord)

--- src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/ScaleClipRenderer.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/ScaleClipRenderer.java
@@ -3,7 +3,9 @@ package io.github.bmb0136.maestro.timeline.clip;
 import io.github.bmb0136.maestro.core.clip.ScaleClip;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import org.jetbrains.annotations.NotNull;
 
 public class ScaleClipRenderer {
@@ -16,5 +18,25 @@ public class ScaleClipRenderer {
      * @param baseColor The primary accent color to render things in
      */
     public static void render(@NotNull ScaleClip clip, @NotNull GraphicsContext gc, @NotNull Rectangle2D area, @NotNull Color baseColor) {
+    gc.save();
+    gc.setFill(baseColor.darker());
+    gc.fillRect(area.getMinX(), area.getMinY(), area.getWidth(), area.getHeight());
+    Color textColor = Color.WHITE;
+        UpdateModifierCount(clip, gc, area, textColor);
+        gc.restore();
+    }
+    private static void UpdateModifierCount(ScaleClip clip, @NotNull GraphicsContext gc, @NotNull Rectangle2D area, @NotNull Color color) {
+        gc.clearRect(0, 0, area.getWidth(), area.getHeight());
+        gc.setFill(color);
+
+        //Label for Modifier
+        Label CountModifiers = new Label( + clip.getModifiers().size() + "M");
+
+        CountModifiers.setFont(gc.getFont());
+        Font font = new Font(gc.getFont().getName(), 12 + (Math.sqrt(area.getHeight() / 4)));
+        gc.setFont(font);
+
+        //Only needs the Width of the new Rectangle to stay within Y-Boundaries
+        gc.fillText(CountModifiers.getText(), area.getMaxX() - 25, area.getMinY(), area.getWidth());
     }
 }
