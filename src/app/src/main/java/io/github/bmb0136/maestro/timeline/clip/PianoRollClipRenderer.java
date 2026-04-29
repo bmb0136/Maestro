a3324a0597a071ef1aa5f276e0acf737f6292f05
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-29 18:30:38 -0500

    Fix #73

--- src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/PianoRollClipRenderer.java
+++ src/app/src/main/java/io/github/bmb0136/maestro/timeline/clip/PianoRollClipRenderer.java
@@ -17,47 +17,38 @@ public class PianoRollClipRenderer {
 	 * @param baseColor The primary accent color to render things in
 	 */
 	public static void render(@NotNull PianoRollClip clip, @NotNull GraphicsContext gc, @NotNull Rectangle2D area, @NotNull Color baseColor) {
+		if (!clip.iterator().hasNext()) {
+			return;
+		}
 
-		PianoUpdater(clip, gc, area, baseColor = Color.BLACK);
-	}
-
-	public static void PianoUpdater(@NotNull PianoRollClip clip, @NotNull GraphicsContext gc, @NotNull Rectangle2D area, @NotNull Color baseColor) {
-		int minMidi = 9999;
-		int maxMidi = -9999;
+		int minMidi = Integer.MAX_VALUE;
+		int maxMidi = Integer.MIN_VALUE;
 		for (Note note : clip) {
-			if (maxMidi < note.pitch().toMidi()) {
-				maxMidi = note.pitch().toMidi();
-			} else if (minMidi > note.pitch().toMidi()) {
-				minMidi = note.pitch().toMidi();
+			var midi = note.pitch().toMidi();
+
+			if (midi > maxMidi) {
+				maxMidi = midi;
+			}
+
+			if (midi < minMidi) {
+				minMidi = midi;
 			}
 		}
-		if (minMidi == maxMidi) {
-			minMidi -= 10;
-			maxMidi += 10;
+		final int MIN_SCALE = 10;
+		while (maxMidi - minMidi < MIN_SCALE) {
+			maxMidi++;
+			minMidi--;
 		}
-		double scale = maxMidi - minMidi;
+		minMidi--;
 
 		gc.setFill(baseColor);
 		for (Note note : clip) {
-			double y = ((double) (note.pitch().toMidi() - minMidi) / (scale + 1) * (area.getMinY() - area.getMaxY())) + area.getMaxY();
-			double height = area.getHeight() / (scale + 1);
+			double y = ((double) (note.pitch().toMidi() - minMidi) / (maxMidi - minMidi) * (area.getMinY() - area.getMaxY())) + area.getMaxY();
+			double height = area.getHeight() / (maxMidi - minMidi);
 
-			double x = note.position() / clip.getDuration() * area.getWidth();
+			double x = (note.position() / clip.getDuration() * area.getWidth()) + area.getMinX();
 			double width = note.duration() / clip.getDuration() * area.getWidth();
 			gc.fillRect(x, y, width, height);
 		}
 	}
-        /* Brandon's Code
-        if (minMidi == maxMidi) {
-            minMidi -= 10;
-            maxMidi += 10;
-        }
-        double y = ((double)(note.pitch().toMidi() - minMidi) / (maxMidi - minMidi + 1) * (area.getMinY() - area.getMaxY())) + area.getMaxY();
-        double height = area.getHeight() / (maxMidi - minMidi + 1);
-        double x = note.position() / clip.getDuration() * area.getWidth();
-        double width = note.duration() / clip.getDuration() * area.getWidth();
-    }
-
-       */
-
 }
